b733050dbc533c151f4b974e0a38310973f61c12
alex <alexreveles@yahooo.com>
2021-01-30 01:19:12 -0800

    update moment js

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@ const cities = [];
 
 
 let currentDay = moment().format('MM/DD/YYYY');
+let todays =  moment().add(0, 'days').format('MM/DD/YYYY');
 let tomorrow = moment().add(1, 'days').format('MM/DD/YYYY');
 let twoDays = moment().add(2, 'days').format('MM/DD/YYYY');
 let threeDays = moment().add(3, 'days').format('MM/DD/YYYY');
@@ -94,6 +95,7 @@ $(document).ready(function () {
           .then(function (data) {
             console.log(data);
 
+            let todays = document.createElement('p')
             let weatherIcon = document.createElement("img");
             let temp = document.createElement("p");
             let humid = document.createElement("p");
@@ -111,11 +113,13 @@ $(document).ready(function () {
 
 
             );
+            todays.textContent = "Todays Date: " + currentDay.todays;
             temp.textContent = "Temperature: " + data.current.temp + " ºf";
             humid.textContent = "Humidity: " + data.current.humidity;
             windSpeed.textContent = "Wind speed: " + data.current.wind_speed;
             UVIndex.textContent = "UV Index: " + data.current.uvi;
 
+            currentWeatherEl.appendChild(todays)
             currentWeatherEl.appendChild(weatherIcon);
             currentWeatherEl.appendChild(temp);
             currentWeatherEl.appendChild(humid);
@@ -143,6 +147,7 @@ $(document).ready(function () {
 
 
         ////////cast for tomorrow weather//////////
+        
         tomorrowdate = document.createElement('p');
         tomorrowdate.textContent = tomorrow;
         fiveDayForecast.appendChild(tomorrowdate);
@@ -161,13 +166,16 @@ $(document).ready(function () {
         tomorrowHum.textContent = data.list[4].main.humidity
         fiveDayForecast.appendChild(tomorrowHum);
 
-        /// two days date 
+        /// two days  
 
         twoDaysdate = document.createElement('p');
         twoDaysdate.textContent = twoDays
         fiveDayForecast.appendChild(twoDaysdate);
 
-        // icon twoDaysIcon
+        
+        twoDaysIcon = document.createElement('img');
+        twoDaysIcon.setAttribute('src', 'http://openweathermap.org/img/wn/' + data.list[11].weather[0].icon + '@2x.png' )
+        fiveDayForecast.appendChild(twoDaysIcon);
 
 
         twoDaysTemp = document.createElement('p');
@@ -178,7 +186,44 @@ $(document).ready(function () {
         twoDaysHum.textContent = data.list[11].main.humidity
         fiveDayForecast.appendChild(twoDaysHum);
 
+        // three days
 
+        threeDaysdate = document.createElement('p');
+        threeDaysdate.textContent = threeDays
+        fiveDayForecast.appendChild(threeDaysdate);
+
+        threeDaysIcon = document.createElement('img');
+        threeDaysIcon.setAttribute('src', 'http://openweathermap.org/img/wn/' + data.list[33].weather[0].icon + '@2x.png' )
+        fiveDayForecast.appendChild(threeDaysIcon);
+
+
+        threeDaysTemp = document.createElement('p');
+        threeDaysTemp.textContent = data.list[33].main.temp
+        fiveDayForecast.appendChild(threeDaysTemp);
+
+        threeDaysHum = document.createElement('p');
+        threeDaysHum.textContent = data.list[33].main.humidity
+        fiveDayForecast.appendChild(threeDaysHum);
+
+        // four days
+
+       
+        fourDaysdate = document.createElement('p');
+        fourDaysdate.textContent = fourDays
+        fiveDayForecast.appendChild(fourDaysdate);
+
+        fourDaysIcon = document.createElement('img');
+        fourDaysIcon.setAttribute('src', 'http://openweathermap.org/img/wn/' + data.list[37].weather[0].icon + '@2x.png' )
+        fiveDayForecast.appendChild(fourDaysIcon);
+
+
+        fourDaysTemp = document.createElement('p');
+        fourDaysTemp.textContent = data.list[37].main.temp
+        fiveDayForecast.appendChild(fourDaysTemp);
+
+        fourDaysHum = document.createElement('p');
+        fourDaysHum.textContent = data.list[37].main.humidity
+        fiveDayForecast.appendChild(fourDaysHum);
 
         
 
